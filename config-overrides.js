7ce755ecbe7fbfc5e5fb30c5b2475953f84067d8
Mridul Kumar <kumar.mridul000@gmail.com>
2022-10-05 03:45:09 +0530

    node polyfill plugin for webpack 5, apollo-link-token-refresh error fix

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,22 @@
-module.exports = function override(webpackConfig) {
-  webpackConfig.module.rules.push({
-    test: /\.mjs$/,
-    include: /node_modules/,
-    type: "javascript/auto"
-  });
+// Copyright 2019-2020 @Premiurly/polkassembly authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
 
-  return webpackConfig;
-}
+/* eslint-disable no-tabs */
+/* eslint-disable sort-keys */
+
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+
+module.exports = function override(webpackConfig) {
+	webpackConfig.module.rules.push({
+		test: /\.mjs$/,
+		include: /node_modules/,
+		type: 'javascript/auto'
+	});
+
+	webpackConfig.plugins.push(
+		new NodePolyfillPlugin()
+	);
+
+	return webpackConfig;
+};
