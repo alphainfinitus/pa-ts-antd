c7ecaf5de74a800c3c77e34e7802fbea9d1e5a53
Mridul Kumar <kumar.mridul000@gmail.com>
2022-10-27 23:41:05 +0530

    rest of listing and post pages

--- src/components/AppLayout/SwitchRoutes.tsx
+++ src/components/AppLayout/SwitchRoutes.tsx
@@ -6,15 +6,23 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from 'src/screens/Home';
 import Bounties from 'src/screens/Listing/Bounties';
+import ChildBounties from 'src/screens/Listing/ChildBounties';
 import Discussions from 'src/screens/Listing/Discussions';
+import Motions from 'src/screens/Listing/Motions';
 import Proposals from 'src/screens/Listing/Proposals';
 import Referenda from 'src/screens/Listing/Referenda';
+import TechCommProposals from 'src/screens/Listing/TechCommProposals';
+import Tips from 'src/screens/Listing/Tips';
 import Treasury from 'src/screens/Listing/Treasury';
 import LoginForm from 'src/screens/LoginForm';
 import BountyPost from 'src/screens/Posts/BountyPost';
+import ChildBountyPost from 'src/screens/Posts/ChildBountyPost';
 import DiscussionPost from 'src/screens/Posts/DiscussionPost';
+import MotionPost from 'src/screens/Posts/MotionPost';
 import ProposalPost from 'src/screens/Posts/ProposalPost';
 import ReferendumPost from 'src/screens/Posts/ReferendumPost';
+import TechCommProposalPost from 'src/screens/Posts/TechCommProposalPost';
+import TipPost from 'src/screens/Posts/TipPost';
 import TreasuryPost from 'src/screens/Posts/TreasuryPost';
 import RequestResetPassword from 'src/screens/RequestResetPassword';
 
@@ -41,6 +49,19 @@ const SwitchRoutes = () => {
 
 			<Route path="/bounties" element={<Bounties />} />
 			<Route path="/bounty/:id" element={<BountyPost />} />
+
+			<Route path="/child_bounties" element={<ChildBounties />} />
+			<Route path="/child_bounty/:id" element={<ChildBountyPost />} />
+
+			<Route path="/tips" element={<Tips />} />
+			<Route path="/tip/:hash" element={<TipPost />} />
+
+			<Route path="/motions" element={<Motions />} />
+			<Route path="/motion/:id" element={<MotionPost />} />
+
+			<Route path="/tech-comm-proposals" element={<TechCommProposals />} />
+			<Route path="/tech/:id" element={<TechCommProposalPost />} />
+
 		</Routes>
 	);
 };
---
+++ src/components/Listing/TechCommProposals/TechCommProposalsContainer.tsx
@@ -0,0 +1,29 @@
+// Copyright 2019-2020 @Premiurly/polkassembly authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+import React from 'react';
+import { useTechCommitteeProposalCountQuery } from 'src/generated/graphql';
+import { post_type } from 'src/global/post_types';
+
+import TechCommProposalsListingWrapper from './TechCommProposalsListingWrapper';
+
+const TechCommProposalsContainer = ({ className } : { className?:string }) => {
+	// TODO: Enable Refetch
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	const { data, refetch } = useTechCommitteeProposalCountQuery({ variables: {
+		postType: post_type.ON_CHAIN
+	} });
+
+	return (
+		<div className={`${className} shadow-md bg-white p-3 md:p-8 rounded-md`}>
+			<div className='flex items-center justify-between'>
+				<h1 className='dashboard-heading'>{ data?.posts_aggregate.aggregate?.count } Tech Comm. Proposals</h1>
+			</div>
+
+			<TechCommProposalsListingWrapper className='mt-6' count={data?.posts_aggregate.aggregate?.count} />
+		</div>
+	);
+};
+
+export default TechCommProposalsContainer;
