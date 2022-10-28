1208b5fdf89e9f262a8c077a23f3cb841e372f8c
Mridul Kumar <kumar.mridul000@gmail.com>
2022-10-28 09:32:55 +0530

    fixed sidebar

--- src/components/AppLayout/NavHeader.tsx
+++ src/components/AppLayout/NavHeader.tsx
@@ -18,7 +18,7 @@ interface Props {
 
 const NavHeader = ({ sidebarCollapsed, setSidebarCollapsed } : Props) => {
 	return (
-		<Header className='flex items-center bg-white h-[60px] max-h-[60px] px-6'>
+		<Header className='flex items-center bg-white h-[60px] max-h-[60px] px-6 z-50'>
 			<MenuOutlined className='lg:hidden mr-5' onClick={() => setSidebarCollapsed(!sidebarCollapsed)} />
 			<nav className='w-full lg:w-5/6 lg:mx-auto flex items-center justify-between'>
 				<Link className='flex' to='/'><PALogoBlack /></Link>
