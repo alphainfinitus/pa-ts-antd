376430a9603ad4b6edb577016037459c4ef0e237
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-10-30 08:12:26 +0530

    feat(ExtensionNotFound): new comp added

--- src/components/Signup/Web3Signup.tsx
+++ src/components/Signup/Web3Signup.tsx
@@ -21,13 +21,13 @@ import AuthForm from 'src/ui-components/AuthForm';
 import FilteredError from 'src/ui-components/FilteredError';
 import Loader from 'src/ui-components/Loader';
 import getEncodedAddress from 'src/util/getEncodedAddress';
-import getExtensionUrl from 'src/util/getExtensionUrl';
 import getNetwork from 'src/util/getNetwork';
 
 import { ReactComponent as NovaWalletIcon } from '../../assets/wallet/nova-wallet-star.svg';
 import { ReactComponent as PolkadotJSIcon } from '../../assets/wallet/polkadotjs-icon.svg';
 import { ReactComponent as SubWalletIcon } from '../../assets/wallet/subwallet-icon.svg';
 import { ReactComponent as TalismanIcon } from '../../assets/wallet/talisman-icon.svg';
+import ExtensionNotDetected from '../ExtensionNotDetected';
 
 interface Props {
   chosenWallet: Wallet;
@@ -238,30 +238,7 @@ const Web3Signup: FC<Props> = ({
 			<AuthForm onSubmit={handleSignup} className="flex flex-col gap-y-6">
 				{extensionNotFound?
 					<div className='flex justify-center items-center my-5'>
-						<Alert
-							message={
-								<div className='flex gap-x-2'>
-									<span className='capitalize'>
-										{chosenWallet? chosenWallet: 'Wallet'}
-									</span>
-									<span>
-										extension not detected.
-									</span>
-								</div>
-							}
-							description={
-								getExtensionUrl() ?
-									<div className='max-w-md'>
-				No web 3 account integration could be found. To be able to vote on-chain, visit this page on a computer with polkadot-js extension.
-									</div>
-									:
-									<div className='max-w-md'>
-				Please install <a href='https://www.mozilla.org/en-US/firefox/'>Firefox</a> or <a href='https://www.google.com/chrome/'>Chrome</a> browser to use this feature.
-									</div>
-							}
-							type="info"
-							showIcon
-						/>
+						<ExtensionNotDetected chosenWallet={chosenWallet} />
 					</div>
 					: null
 				}
