1121328a6359034f64994a4339b64e6cbb3ef5de
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-10-31 08:14:59 +0530

    feat(Settings): address comp created

--- src/screens/Settings/Account/Address.tsx
+++ src/screens/Settings/Account/Address.tsx
@@ -1,15 +1,44 @@
 // Copyright 2019-2020 @Premiurly/polkassembly authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
-import React, { FC } from 'react';
+import { web3Accounts, web3Enable } from '@polkadot/extension-dapp';
+import { InjectedAccountWithMeta } from '@polkadot/extension-inject/types';
+import React, { FC, useEffect, useState } from 'react';
+import ExtensionNotDetected from 'src/components/ExtensionNotDetected';
+import getNetwork from 'src/util/getNetwork';
 
 interface Props {
 
 }
 
 const Address: FC<Props> = () => {
+	const [, setAccounts] = useState<InjectedAccountWithMeta[]>([]);
+	const [extensionNotAvailable, setExtensionNotAvailable] = useState(false);
+
+	const refetch = async () => {
+		const extensions = await web3Enable(getNetwork());
+
+		if (extensions.length === 0) {
+			setExtensionNotAvailable(true);
+			return;
+		} else {
+			setExtensionNotAvailable(false);
+		}
+		const allAccounts = await web3Accounts();
+
+		setAccounts(allAccounts);
+	};
+	useEffect(() => {
+		refetch();
+	}, []);
 	return (
-		<article className='mb-8'>Address</article>
+		<article className='mb-8'>
+			{
+				extensionNotAvailable
+					? <div className='max-w-[600px]'><ExtensionNotDetected /></div>
+					: <div></div>
+			}
+		</article>
 	);
 };
 
--- src/screens/Settings/Account/index.tsx
+++ src/screens/Settings/Account/index.tsx
@@ -1,8 +1,6 @@
 // Copyright 2019-2020 @Premiurly/polkassembly authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
-
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import styled from '@xstyled/styled-components';
 import { Button, Form, Row, Switch } from 'antd';
 import React, { FC, useState } from 'react';
@@ -14,15 +12,17 @@ import MultiSignatureAddress from './MultiSignatureAddress';
 interface IAddressHeaderProps {
     header?: string;
     id?: string;
+    checked?: boolean;
+    onChange?: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-const AddressHeader: FC<IAddressHeaderProps> = ({ header, id }) => {
+const AddressHeader: FC<IAddressHeaderProps> = ({ checked, header, id, onChange }) => {
 	return (
 		<article className='flex items-center gap-x-2 text-sm font-normal tracking-wide leading-6 mb-6'>
 			<label className='cursor-pointer' htmlFor={id}>
 				{header}
 			</label>
-			<Switch id={id} size='small' defaultChecked />
+			<Switch checked={checked} onChange={(e) => onChange? onChange(e): null} id={id} size='small' defaultChecked />
 		</article>
 	);
 };
@@ -32,18 +32,20 @@ interface Props {
 }
 
 const Account: FC<Props> = ({ className }) => {
+	const [isLinkAddress, setIsLinkAddress] = useState(false);
+	const [isMultiSigAddress] = useState(false);
 	const [isSave] = useState(false);
 	return (
 		<Row className={`${className} flex flex-col w-full`}>
 			<Header heading='Account Settings' subHeading='Update your account settings' />
 			<Form className='mt-8'>
 				<section>
-					<AddressHeader header='Link Address' id='link_address' />
-					<Address />
+					<AddressHeader checked={isLinkAddress} header='Link Address' id='link_address' onChange={setIsLinkAddress} />
+					{isLinkAddress? <Address />: null}
 				</section>
 				<section>
 					<AddressHeader header='Link Multi Signature Address' id='link_multi_address' />
-					<MultiSignatureAddress/>
+					{isMultiSigAddress? <MultiSignatureAddress/>: null}
 				</section>
 				<Button
 					disabled={!isSave}
