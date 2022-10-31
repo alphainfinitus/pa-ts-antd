ebbd072cfe94db28bb4ac71a717f309d091b5215
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-10-31 15:33:14 +0530

    feat(Settings): multi signature address settings completed

--- src/screens/Settings/Account/index.tsx
+++ src/screens/Settings/Account/index.tsx
@@ -2,7 +2,7 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 import styled from '@xstyled/styled-components';
-import { Button, Form, Row, Switch } from 'antd';
+import { Row, Switch } from 'antd';
 import React, { FC, useState } from 'react';
 import Header from 'src/screens/Settings/Header';
 
@@ -33,29 +33,33 @@ interface Props {
 
 const Account: FC<Props> = ({ className }) => {
 	const [isLinkAddress, setIsLinkAddress] = useState(false);
-	const [isMultiSigAddress] = useState(false);
-	const [isSave] = useState(false);
+	const [isMultiSigAddress, setIsMultiSigAddress] = useState(false);
 	return (
 		<Row className={`${className} flex flex-col w-full`}>
 			<Header heading='Account Settings' subHeading='Update your account settings' />
-			<Form className='mt-8'>
+			<div className='mt-8'>
 				<section>
-					<AddressHeader checked={isLinkAddress} header='Link Address' id='link_address' onChange={setIsLinkAddress} />
+					<AddressHeader
+						checked={isLinkAddress}
+						header='Link Address'
+						id='link_address'
+						onChange={setIsLinkAddress}
+					/>
 					{isLinkAddress? <Address />: null}
 				</section>
 				<section>
-					<AddressHeader header='Link Multi Signature Address' id='link_multi_address' />
-					{isMultiSigAddress? <MultiSignatureAddress/>: null}
+					<AddressHeader
+						checked={isMultiSigAddress}
+						header='Link Multi Signature Address'
+						id='link_multi_address'
+						onChange={setIsMultiSigAddress}
+					/>
+					<MultiSignatureAddress
+						open={isMultiSigAddress}
+						dismissModal={() => setIsMultiSigAddress(false)}
+					/>
 				</section>
-				<Button
-					disabled={!isSave}
-					size='large'
-					htmlType='submit'
-					className={`rounded-lg font-semibold text-lg leading-7 text-white py-3 outline-none border-none px-14 flex items-center justify-center ${isSave?'bg-pink_primary':'bg-icon_grey'}`}
-				>
-                    Save
-				</Button>
-			</Form>
+			</div>
 		</Row>
 	);
 };
